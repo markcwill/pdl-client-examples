777d505e9a19b0bf65ac7bffb0fb8d28798c2b09
Jeremy Fee <jmfee@usgs.gov>
2014-04-05 11:05:29 -0600

    Added indexer configuration

--- IndexerListener.py
+++ IndexerListener.py
@@ -25,12 +25,14 @@ if __name__ == '__main__':
 	product = Product.getProduct()
 	# output parsed product
 	product.display(f)
-	# check if moment tensor is Mww
 	props = product.properties
-	if 'derived-magnitude-type' in props and props['derived-magnitude-type'] == 'Mww':
-		f.write('wphase\n')
-	else:
-		f.write('not wphase\n')
+	# check if moment tensor is Mww
+	if product['type'] == 'moment-tensor':
+		f.write('moment-tensor, ')
+		if 'derived-magnitude-type' in props and props['derived-magnitude-type'] == 'Mww':
+			f.write('wphase\n')
+		else:
+			f.write('not wphase\n')
 	# add a blank line
 	f.write('\n')
 

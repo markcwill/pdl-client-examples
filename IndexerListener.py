58d496e7b4adc3ce20c5a0440df00bbb7d1cfde2
Mark <mark.c.will@gmail.com>
2016-06-07 16:37:18 -0700

    Keep going...

--- IndexerListener.py
+++ IndexerListener.py
@@ -21,30 +21,11 @@ if __name__ == '__main__':
     try:
         PDL_INDEX_CHANNEL = 'pdlindex'
         dt = datetime.datetime.now().isoformat()  
-        # write data to a log file
-        logfile = os.path.join('data', os.path.basename(sys.argv[0]) + '.log')
-        f = open(logfile, 'ab+')
-        # current time
-        f.write('# ' + dt + '\n');
-        # command line arguments
-        f.write('# arguments = ' + ' '.join(sys.argv) + '\n');
         # parse command line arguments
         action = IndexerAction.getIndexerAction()
-        # output parsed action
-        f.write('action=' + action.action + '\n')
         # output associated product
         product = action.product
-        product.display(f)
         props = product.properties
-        # check if moment tensor is Mww
-        if product['type'] == 'moment-tensor':
-            f.write('moment-tensor, ')
-            if 'derived-magnitude-type' in props and props['derived-magnitude-type'] == 'Mww':
-                f.write('wphase\n')
-            else:
-                f.write('not wphase\n')
-        # add a blank line
-        f.write('\n')
         # redis
         logging.debug("Got message {0}, {1}".format(dt, product.code))
         msg = {
